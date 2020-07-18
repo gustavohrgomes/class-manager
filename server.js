f1b012a9e1ebd1de568ffe0a8165a22ad44c285f
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-07-18 19:51:59 -0300

    Criando card de professores

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ server.set('view engine', 'njk');
 
 nunjucks.configure("./views", {
   express: server,
+  autoescape: false,
   noCache: true
 });
 
-server.listen(3000, () => {
+server.listen(5000, () => {
   console.log("server is running!");
 });
