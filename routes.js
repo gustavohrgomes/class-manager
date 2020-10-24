bf1753064adf3f498d7fd728612e739646f301ff
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-10-24 17:32:11 -0300

    Add students routes

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const teachers = require('./controllers/teachers');
+const students = require('./controllers/students')
 
 const routes = express.Router();
 
@@ -8,23 +9,20 @@ routes.get('/', (req, res) => {
 });
 
 routes.get('/teachers', teachers.index);
-
-routes.get('/teachers/create', (req, res) => {
-  return res.render('teachers/create');
-});
-
+routes.get('/teachers/create', teachers.create);
 routes.get('/teachers/:id', teachers.show);
-
 routes.get('/teachers/:id/edit', teachers.edit);
-
 routes.post('/teachers', teachers.post);
-
 routes.put('/teachers', teachers.update);
-
 routes.delete('/teachers/', teachers.delete);
 
-routes.get('/students', (req, res) => {
-  return res.render('students/index');
-});
+
+routes.get('/students', students.index);
+routes.get('/students/create', students.create);
+routes.get('/students/:id', students.show);
+routes.get('/students/:id/edit', students.edit);
+routes.post('/students', students.post);
+routes.put('/students', students.update);
+routes.delete('/students/', students.delete);
 
 module.exports = routes;
